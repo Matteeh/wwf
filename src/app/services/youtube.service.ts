1167d9066d3ed69e907acd9b5e20d150a2465f95
mathias <mathias.rahikainen@gmail.com>
2020-07-02 08:43:39 +0300

    Update firebase config so that it works with angular router

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
-import { YOUTUBE_API_KEY } from "../helpers/api_keys";
+import { YOUTUBE_API_KEY } from "../../helpers/api_keys";
 
 @Injectable({
   providedIn: "root",
